1f00ca9bc2e218b7f1dd9502abdd4b36abf717d9
Hyobin Cho <hyobicho@c1r5s1.42seoul.kr>
2023-02-15 19:06:36 +0900

    zoom in/out not working...

--- fract-ol/fractol.h
+++ fract-ol/fractol.h
@@ -6,7 +6,7 @@
 /*   By: hyobicho <hyobicho@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 12:40:58 by hyobicho          #+#    #+#             */
-/*   Updated: 2023/02/14 22:05:27 by hyobicho         ###   ########.fr       */
+/*   Updated: 2023/02/15 19:03:54 by hyobicho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,23 @@
 # define WIDTH 800
 # define HEIGHT 800
 # define N_MAX 128
-# define X_MAX 2
-# define Y_MAX 2
-# define X_MIN -2
-# define Y_MIN -2
+# define ESC 53
+# define LEFT 123
+# define RIGHT 124
+# define DOWN 125
+# define UP 126
+# define SCROLL_UP 4
+# define SCROLL_DOWN 5
 
 typedef struct	s_data {
+	void	*mlx;
+	void	*win;
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
+	double	boundary;
 }	t_data;
 
 typedef struct s_coord
@@ -42,9 +48,6 @@ typedef struct s_coord
 	double	y;
 }	t_coord;
 
-typedef struct	s_vars {
-	void	*mlx;
-	void	*win;
-}	t_vars;
+void	mandelbrot(t_data *data);
 
 #endif
--- fract-ol/mlx_test.c
+++ fract-ol/mlx_test.c
@@ -6,17 +6,42 @@
 /*   By: hyobicho <hyobicho@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 12:40:52 by hyobicho          #+#    #+#             */
-/*   Updated: 2023/02/14 22:12:02 by hyobicho         ###   ########.fr       */
+/*   Updated: 2023/02/15 19:05:49 by hyobicho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int	deal_key()
+int	deal_key(int button, t_data *ptr)
 {
-	// if key가 esc면 프로그램 종료
-	exit(EXIT_SUCCESS);
+	// if key가 esc면 window 닫음
+	if (button == ESC)
+		mlx_destroy_window(ptr->mlx, ptr->win);
+	// else if (button == LEFT)
+	// 방향 이동..
+	return (0);
 }
+
+// int	deal_mouse(int button, int x, int y, t_data *data)
+int	deal_mouse(int button, int x, int y, t_data *data)
+{
+	// 확대 -> 경계값 작아짐, 축소 -> 경계값 커짐  ==> 적용이 안 됨..
+	if (button == SCROLL_UP)
+	{
+		// 배율.. 어떻게..??
+		data->boundary *= 0.9;
+		printf("(%d, %d)\n", x, y);
+		printf("boundary: %f\n", data->boundary);
+	}
+	else if (button == SCROLL_DOWN)
+	{
+		data->boundary += data->boundary * 0.05;
+		printf("(%d, %d)\n", x, y);
+		printf("boundary: %f\n", data->boundary);
+	}
+	return (0);
+}
+
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -46,12 +71,6 @@ int	get_depth(double x, double y, t_coord c)
 	return (n);
 }
 
-// void	get_coord(int h, int w, t_coord *c)
-// {
-// 	c->x = w + (4.0 / WIDTH) * w - 2.0; 
-// 	c->y = h + (4.0 / HEIGHT) * h - 2.0; 
-// }
-
 void	mandelbrot(t_data *data)
 {
 	t_coord c;
@@ -60,53 +79,103 @@ void	mandelbrot(t_data *data)
 	int 	n;
 
 	x = 0;
-	while (x < HEIGHT)
+	while (x < WIDTH)
 	{
-		c.x = - 2 + (4.0 / HEIGHT) * x;
+		c.x = - data->boundary + (data->boundary * 2 / WIDTH) * x;
 		y = 0;
-		while (y < WIDTH)
+		while (y < HEIGHT)
 		{
-			c.y =  2 - (4.0 / WIDTH) * y;
+			c.y =  data->boundary - (data->boundary * 2 / HEIGHT) * y;
 			n = get_depth(c.x, c.y, c);
 			// if (n)
-				// printf("(%d, %d) => (%f, %f), n: %d\n", x, y, c.x, c.y, n);
-			// if (n < 30)
-			// 	my_mlx_pixel_put(data, x, y, 0x606060);
-			// else if (n < 60)
-			// 	my_mlx_pixel_put(data, x, y, 0xccffff);
-			// else if (n < 100)
-			// 	my_mlx_pixel_put(data, x, y, 0x99cfee);
-			// else
-				my_mlx_pixel_put(data, x, y, 1343 * n);
+			// 	printf("(%d, %d) => (%f, %f), n: %d\n", x, y, c.x, c.y, n);
+			my_mlx_pixel_put(data, x, y, 0xffffff - 0x0c0300 * n);
 			y++;
 		}
 		x++;
 	}
 }
 
-// void terminate()
+// 아직 아무것도 안 함
+// void	julia(t_data *data)
 // {
-// 	exit()
+// 	t_coord c;
+// 	int		x;
+// 	int		y;
+// 	int 	n;
+
+// 	x = 0;
+// 	while (x < WIDTH)
+// 	{
+// 		c.x = - 2 + (4.0 / WIDTH) * x;
+// 		y = 0;
+// 		while (y < HEIGHT)
+// 		{
+// 			c.y =  2 - (4.0 / HEIGHT) * y;
+// 			n = get_depth(c.x, c.y, c);
+// 			my_mlx_pixel_put(data, x, y, 0xffffff - 0x0c0300 * n);
+// 			y++;
+// 		}
+// 		x++;
+// 	}
 // }
 
-int main()
+int	ft_strcmp(const char *s1, const char *s2)
 {
-	t_vars ptr;
-	t_data	img;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	i = 0;
+	while (str1[i] || str2[i])
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
+}
+
+int	fractal_type(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		if (ft_strcmp(argv[1], "Mandelbrot") == 0)
+			return (1);
+		if (ft_strcmp(argv[1], "Julia") == 0)
+			return (2);
+		printf("Mandelbrot\n");
+		printf("Julia\n");
+	}	
+	exit(EXIT_FAILURE);
+}
+
+int main(int argc, char **argv)
+{
+	t_data	data;
+	int		f_type;
+
+	data.boundary = 2;
+	// check para
+	f_type = fractal_type(argc, argv);
 	// set up the connection to the display
 	// non-null pointer is returned as a connection identifier
-	ptr.mlx = mlx_init();
+	data.mlx = mlx_init();
 	// manage windows
-	ptr.win = mlx_new_window(ptr.mlx, WIDTH, HEIGHT, "my fract-ol");
-	img.img = mlx_new_image(ptr.mlx, WIDTH, HEIGHT);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	mandelbrot(&img);
-	mlx_put_image_to_window(ptr.mlx, ptr.win, img.img, 0, 0);
-	// mlx_key_hook(ptr.win, deal_key, &ptr);
-	// mlx_mouse_hook(ptr.win, terminate, &ptr);
+	data.win = mlx_new_window(data.mlx, WIDTH, HEIGHT, "my fract-ol");
+	data.img = mlx_new_image(data.mlx, WIDTH, HEIGHT);
+	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
+	mlx_key_hook(data.win, deal_key, &data);
+	if (f_type == 1)
+		mandelbrot(&data);
+	mlx_mouse_hook(data.win, deal_mouse, &data);
+	mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
+	// else
+	// 	julia(&data);
 	// handle keyboard or mouse events
 	// infinite loop that waits for an event
-	mlx_loop(ptr.mlx);
+	mlx_loop(data.mlx);
 	return (0);
 }
