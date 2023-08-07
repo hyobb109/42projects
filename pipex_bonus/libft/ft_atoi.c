041698ca7313d5013141bfb9b89fa82b105e7bd7
Hyobin Cho <hyobicho@c1r6s1.42seoul.kr>
2023-08-07 20:12:54 +0900

    pipex bonus directory created

---
+++ pipex_bonus/libft/ft_atoi.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyobicho <hyobicho@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/08 04:50:31 by hyobicho          #+#    #+#             */
+/*   Updated: 2023/03/02 11:42:55 by hyobicho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	whitespace(char a)
+{
+	if ((a >= 9 && a <= 13) || a == 32)
+		return (1);
+	return (0);
+}
+
+static int	overflow(long long res, int len, int flag)
+{
+	if (flag)
+		len--;
+	if (len > 10 || res < -2147483648 || res > 2147483647)
+		return (1);
+	return (0);
+}
+
+static int	check_str(const char *str, int *sign, int *flag)
+{
+	int	i;
+
+	i = 0;
+	while (whitespace(str[i]))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		*flag = 1;
+		if (str[i] == '-')
+			*sign = -1;
+		i++;
+	}
+	return (i);
+}
+
+int	ft_atoi(const char *str)
+{
+	long long	res;
+	int			sign;
+	int			i;
+	int			flag;
+
+	res = 0;
+	sign = 1;
+	flag = 0;
+	i = check_str(str, &sign, &flag);
+	if (!ft_isdigit(str[i]))
+		ft_error("NaN");
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	if (overflow(sign * res, i, flag) || str[i])
+		ft_error("Overflow");
+	return (res * sign);
+}
