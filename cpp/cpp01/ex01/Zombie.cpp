3498e54b3a55e436194304214eddfe21ad7529d4
Hyobin Cho <hyobicho@student.42seoul.kr>
2023-10-03 16:49:51 +0900

    ex01 컴파일 에러 수정

--- cpp/cpp01/ex01/Zombie.cpp
+++ cpp/cpp01/ex01/Zombie.cpp
@@ -1,17 +1,11 @@
 #include "Zombie.hpp"
 
-Zombie::Zombie() {};
+Zombie::Zombie(){};
 
-Zombie::Zombie(std::string newName) : name(name) {};
+Zombie::Zombie(std::string newName) : name(newName){};
 
-Zombie::~Zombie() {
-    std::cout << name << " got destroyed" << std::endl;
-}
+Zombie::~Zombie() { std::cout << name << " got destroyed" << std::endl; }
 
-void Zombie::announce() {
-    std::cout << name << ": " << MESSAGE << std::endl;
-}
+void Zombie::announce() { std::cout << name << ": " << MESSAGE << std::endl; }
 
-void Zombie::setName(std::string newName) {
-    name = newName;
-}
+void Zombie::setName(std::string newName) { name = newName; }
