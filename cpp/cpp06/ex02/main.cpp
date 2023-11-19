a6414e995d41a0c1ae66f1f7285e0990ac9ab9ab
hyobicho <hyobicho@student.42seoul.kr>
2023-11-20 05:26:09 +0900

    [cpp06] ex02 끝

--- cpp/cpp06/ex02/main.cpp
+++ cpp/cpp06/ex02/main.cpp
@@ -1,7 +1,70 @@
-#include "Base.hpp"
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
 
-Base* generate();
-void identify(Base* p);
-void identify(Base& p);
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
 
-int main() {}
+enum TYPE { A_CLASS, B_CLASS, C_CLASS };
+
+// randomly instanciates A, B or C
+// and returns the instance as a Base pointer
+Base* generate() {
+  int type = std::rand() % 3;
+
+  switch (type) {
+    case A_CLASS:
+      return new A();
+    case B_CLASS:
+      return new B();
+    case C_CLASS:
+      return new C();
+    default:
+      return NULL;
+  }
+}
+
+// prints the actual type of the object pointed to by p: "A", "B" or "C".
+void identify(Base* p) {
+  if (dynamic_cast<A*>(p) != NULL) {
+    std::cout << "A" << std::endl;
+  } else if (dynamic_cast<B*>(p) != NULL) {
+    std::cout << "B" << std::endl;
+  } else if (dynamic_cast<C*>(p) != NULL) {
+    std::cout << "C" << std::endl;
+  }
+}
+
+void identify(Base& p) {
+  try {
+    A& a = dynamic_cast<A&>(p);
+    (void)a;
+    std::cout << "A" << std::endl;
+  } catch (std::exception& e) {
+  }
+  try {
+    B& b = dynamic_cast<B&>(p);
+    (void)b;
+    std::cout << "B" << std::endl;
+  } catch (std::exception& e) {
+  }
+  try {
+    C& c = dynamic_cast<C&>(p);
+    (void)c;
+    std::cout << "C" << std::endl;
+  } catch (std::exception& e) {
+  }
+}
+
+int main() {
+  std::srand(static_cast<unsigned int>(std::time(NULL)));
+
+  std::cout << "===Generating...===" << std::endl;
+  Base* ptr = generate();
+  std::cout << "Pointer Type: ";
+  identify(ptr);
+  std::cout << "Reference Type: ";
+  identify(*ptr);
+  return 0;
+}
--- cpp/cpp06/ex02/utils.cpp
+++
@@ -1,7 +0,0 @@
-#include "Base.hpp"
-
-Base* generate() {}
-
-void identify(Base* p) {}
-
-void identify(Base& p) {}
