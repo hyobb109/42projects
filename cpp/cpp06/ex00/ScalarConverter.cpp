4c96abefc9921f3ba16d5d62d9001600c3fe6edd
Hyobin Cho <hyobicho@student.42seoul.kr>
2023-11-20 22:01:15 +0900

    [cpp06] ex00 하는 중

--- cpp/cpp06/ex00/ScalarConverter.cpp
+++ cpp/cpp06/ex00/ScalarConverter.cpp
@@ -1,21 +1,70 @@
 #include "ScalarConverter.hpp"
 
-// first detect the type of the literal passed as parameter
+ScalarConverter::ScalarConverter() {}
+
+ScalarConverter::~ScalarConverter() {}
+
+ScalarConverter::ScalarConverter(const ScalarConverter& s) { (void)s; }
+
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter& s) {
+  (void)s;
+  return *this;
+}
+
+void ScalarConverter::notANumber() {
+  std::cout << "char: impossible" << std::endl;
+  std::cout << "int: impossible" << std::endl;
+  std::cout << "float: nanf" << std::endl;
+  std::cout << "double: nan" << std::endl;
+}
+
+void ScalarConverter::detectType(std::string str) {
+  std::size_t found = str.find_first_not_of("0123456789");
+  // digit 으로만 이루어져 있으면 int or char
+  if (found == std::string::npos) {
+    convertInt(str.c_str());
+  } else if (str[found] == '.') {
+    std::cout << "* " << str.substr(found + 1) << std::endl;
+  }
+}
+
+void ScalarConverter::convertInt(const char* str) {
+  std::cout << str << std::endl;
+}
+
+// void ScalarConverter::convertChar(const char* str) {}
+
+// void ScalarConverter::convertFloat(const char* str) {}
+
+// void ScalarConverter::convertDouble(const char* str) {}
+
 // convert it from string to its actual type
 // convert it explicitly to the three other data typed
 // display the results
 // if conversion doesn't make any sencse or overflows => display impossible
-void ScalarConverter::convert(std::string n) {
-  int int_num;
-  // double d_num;
-  float f_num = 0.0f;
-  std::stringstream ss(n);
-  while (ss >> int_num) {
-    std::cout << int_num << std::endl;
-  }
+void ScalarConverter::convert(std::string str) {
+  char** endptr = NULL;
+  double d_num = 0;
+  // int decimal = 0;
 
+  // first detect the type of the literal passed as parameter
+  detectType(str);
+  if (str.find_first_of('f') == str.length() - 1) {
+    d_num = atof(str.c_str());
+    std::cout << std::fixed << std::setprecision(4) << "fnum: " << d_num
+              << std::endl;
+  } else {
+    d_num = strtod(str.c_str(), endptr);
+    std::cout << std::fixed << std::setprecision(4) << "num: " << d_num
+              << std::endl;
+  }
+  std::cout << endptr << "\n";
+  if (endptr) {
+    notANumber();
+    return;
+  }
   // std::cout << "char " << int_num << std::endl;
   // std::cout << "int: " << int_num << std::endl;
   // std::cout << "float: " << d_num << std::endl;
-  std::cout << "double: " << f_num << 'f' << std::endl;
+  // std::cout << "double: " << f_num << 'f' << std::endl;
 }
--- cpp/cpp06/ex00/ScalarConverter.hpp
+++ cpp/cpp06/ex00/ScalarConverter.hpp
@@ -2,18 +2,29 @@
 #define SCALAR_CONVERTER_HPP
 
 #include <cctype>
+#include <cstdlib>
+#include <iomanip>
 #include <iostream>
 #include <sstream>
 #include <string>
+
+enum TYPE { CHAR, INT, FLOAT, DOUBLE };
+
 class ScalarConverter {
  private:
   ScalarConverter();
   ~ScalarConverter();
   ScalarConverter(const ScalarConverter& s);
   ScalarConverter& operator=(const ScalarConverter& s);
+  static void notANumber();
+  static void detectType(std::string str);
+  static void convertInt(const char* str);
+  static void convertChar(const char* str);
+  static void convertFloat(const char* str);
+  static void convertDouble(const char* str);
 
  public:
-  static void convert(std::string n);
+  static void convert(std::string str);
 };
 
 #endif
