8d84fa5da49f6006fe622f0f15207e09b3d5f624
Hyobin Cho <hyobicho@student.42seoul.kr>
2023-11-24 19:01:33 +0900

    [cpp07] ex02

--- cpp/cpp07/ex02/Array.hpp
+++ cpp/cpp07/ex02/Array.hpp
@@ -1,7 +1,35 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <exception>
 template <typename T>
-class Array {};
+class Array {
+ private:
+  T* data;
+  unsigned int len;
+
+ public:
+  Array() : data(new T[0]), len(0) {}
+  Array(unsigned int n) : data(new T[n]), len(n) {
+    for
+  }
+  Array(const Array& arr) : data(new T[arr.len]), len(arr.len) {
+    for (unsigned int i = 0; i < len; i++) {
+      data[i] = arr[i];
+    }
+  }
+  Array& operator=(const Array& arr) {
+    if (this != &arr) {
+      delete[] data;
+      *this = Array(arr);
+    }
+    return *this;
+  }
+  T& operator[](const unsigned int i) const {
+    if (i >= len) throw std::exception();
+    return data[i];
+  }
+  unsigned int size() const { return len; }
+};
 
 #endif
