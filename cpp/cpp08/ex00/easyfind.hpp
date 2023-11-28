9c8bad5b4de13dadd56aa83ac82bc7b688443ab7
Hyobin Cho <hyobicho@student.42seoul.kr>
2023-11-28 20:34:56 +0900

    [cpp08] ex00 exception 빼고 끝

--- cpp/cpp08/ex00/easyfind.hpp
+++ cpp/cpp08/ex00/easyfind.hpp
@@ -1,7 +1,22 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <exception>
+
 template <typename T>
-void easyfind(T container, int target) {}
+int easyfind(T container, int target) {
+  // find the first occurrence of the target in the container
+  typename T::iterator iter = container.begin();
+
+  while (iter != container.end()) {
+    if (*iter == target) {
+      return *iter;
+    }
+    ++iter;
+  }
+  // if no occurrence is found, throw an exception or return an error value of
+  // your choice
+  throw std::exception();
+}
 
 #endif
--- cpp/cpp08/ex00/main.cpp
+++ cpp/cpp08/ex00/main.cpp
@@ -1,5 +1,35 @@
+
+
+#include <cstdlib>
+#include <deque>
 #include <iostream>
+#include <vector>
 
 #include "easyfind.hpp"
 
-int main() { return 0; }
+int main(int ac, char** av) {
+  if (ac != 2) {
+    std::cout << "Usage: ./a.out <integer>" << std::endl;
+    return 1;
+  }
+
+  int target = std::atoi(av[1]);
+  std::vector<int> v;
+  std::deque<int> d;
+
+  for (int i = 0; i < 10; ++i) {
+    v.push_back(i);
+    d.push_back(i);
+  }
+
+  try {
+    std::cout << "=====vector=====" << std::endl;
+    std::cout << easyfind(v, target) << std::endl;
+    std::cout << "=====deque=====" << std::endl;
+    std::cout << easyfind(d, target) << std::endl;
+  } catch (std::exception& e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  return 0;
+}
