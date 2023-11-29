f1b996500aeecca5aea9fb56cbd87e0a1ee825b2
hyobicho <hyobicho@student.42seoul.kr>
2023-11-29 19:17:18 +0900

    [cpp08] ex02 fillSpan 멤버함수 선언

--- cpp/cpp08/ex01/Span.hpp
+++ cpp/cpp08/ex01/Span.hpp
@@ -6,19 +6,16 @@
 class Span {
  private:
   unsigned int n_;
+  Span();
 
  public:
-  Span();
   Span(unsigned int n);
   Span(const Span& s);
   Span& operator=(const Span& s);
   void addNumber(unsigned int num);
   unsigned int shortestSpan();
   unsigned int longestSpan();
-  class FullException : public std::exception {
-   public:
-    virtual const char* what() const throw();
-  };
+  void fillSpan();
 };
 
 #endif
