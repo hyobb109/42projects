49694cd5786b4a2d958230c161dbd9c3372a53aa
hyobicho <hyobicho@student.42seoul.kr>
2023-11-30 19:30:12 +0900

    [cpp07] ex02 소멸자 추가 및 대입 연산자 오버로딩 수정

--- cpp/cpp07/ex02/Array.hpp
+++ cpp/cpp07/ex02/Array.hpp
@@ -10,27 +10,36 @@ class Array {
 
  public:
   Array() : data(new T[0]), len(0) {}
+
+  ~Array() { delete[] data; }
+
   Array(unsigned int n) : data(new T[n]), len(n) {
     for (unsigned int i = 0; i < n; i++) {
       data[i] = T();
     }
   }
+
   Array(const Array& arr) : data(new T[arr.len]), len(arr.len) {
     for (unsigned int i = 0; i < len; i++) {
       data[i] = arr[i];
     }
   }
+
   Array& operator=(const Array& arr) {
     if (this != &arr) {
       delete[] data;
-      *this = Array(arr);
+      for (unsigned int i = 0; i < len; i++) {
+        data[i] = arr[i];
+      }
     }
     return *this;
   }
+
   T& operator[](const long long i) const {
     if (i < 0 || i >= len) throw std::out_of_range("Out of Range!");
     return data[i];
   }
+
   unsigned int size() const { return len; }
 };
 
--- cpp/cpp07/ex02/main.cpp
+++ cpp/cpp07/ex02/main.cpp
@@ -4,40 +4,46 @@
 
 #define MAX_VAL 10
 
+// void leaks() { system("leaks a.out"); }
+
 int main(int, char**) {
   Array<int> numbers(MAX_VAL);
   int* mirror = new int[MAX_VAL];
   srand(time(NULL));
+  // atexit(leaks);
+
   for (int i = 0; i < MAX_VAL; i++) {
     const int value = rand();
     numbers[i] = value;
     mirror[i] = value;
   }
-  {
-    Array<int> tmp = numbers;
-    Array<int> test(tmp);
-  }
+
+  Array<int> tmp = numbers;
+  Array<int> test(tmp);
 
   for (int i = 0; i < MAX_VAL; i++) {
-    if (mirror[i] != numbers[i]) {
+    std::cout << "numbers[" << i << "]: " << numbers[i] << std::endl;
+    std::cout << "mirror[" << i << "]: " << mirror[i] << std::endl;
+    std::cout << "------------------------\n";
+    if (mirror[i] != numbers[i] || mirror[i] != tmp[i] ||
+        mirror[i] != test[i]) {
       std::cerr << "didn't save the same value!!" << std::endl;
       return 1;
     }
   }
   try {
+    std::cout << "numbers[-2]" << std::endl;
     numbers[-2] = 0;
   } catch (const std::exception& e) {
     std::cerr << e.what() << '\n';
   }
   try {
+    std::cout << "numbers[MAX_VAL]" << std::endl;
     numbers[MAX_VAL] = 0;
   } catch (const std::exception& e) {
     std::cerr << e.what() << '\n';
   }
 
-  for (int i = 0; i < MAX_VAL; i++) {
-    numbers[i] = rand();
-  }
   delete[] mirror;
   return 0;
 }
