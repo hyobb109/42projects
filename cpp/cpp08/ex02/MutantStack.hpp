2c3f6078e0d7bf0990af6b9d506196df4ebfb49a
Hyobin Cho <hyobicho@student.42seoul.kr>
2024-01-04 17:55:39 +0900

    [cpp08] ex02 const_iterator

--- cpp/cpp08/ex02/MutantStack.hpp
+++ cpp/cpp08/ex02/MutantStack.hpp
@@ -8,6 +8,7 @@ template <typename T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container> {
  public:
   typedef typename Container::iterator iterator;
+  typedef typename Container::const_iterator const_iterator;
 
   MutantStack() : std::stack<T, Container>() {}
 
@@ -26,6 +27,10 @@ class MutantStack : public std::stack<T, Container> {
 
   iterator begin() { return this->c.begin(); }
 
+  const_iterator begin() const { return this->c.begin(); }
+
   iterator end() { return this->c.end(); }
+
+  const_iterator end() const { return this->c.end(); }
 };
 #endif
