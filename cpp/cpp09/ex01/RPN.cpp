554f07a23ce2aa26043e06e6655f3eaa5245c560
hyobicho <hyobicho@student.42seoul.kr>
2024-01-14 21:25:20 +0900

    [cpp09] ex01 RPN 하는 중

--- cpp/cpp09/ex01/RPN.cpp
+++ cpp/cpp09/ex01/RPN.cpp
@@ -1,1 +1,17 @@
 #include "RPN.hpp"
+
+RPN::RPN() {}
+
+RPN::~RPN() {}
+
+RPN::RPN(const RPN& copy) {}
+
+RPN& RPN::operator=(const RPN& copy) { return *this; }
+
+void RPN::calculate(std::string input) {
+  std::stack<int> s;
+  // 파싱 오류 -> return;
+  // 0 ~ 9 까지의 숫자, - + / * , 공백만 받음
+  //./RPN "8 9 * 9 - 9 - 9 - 4 - 1 +"
+  // number => push, operator => stack 모든 원소 연산해서 스택 바꿔줌
+}
--- cpp/cpp09/ex01/RPN.hpp
+++ cpp/cpp09/ex01/RPN.hpp
@@ -1,6 +1,18 @@
 #ifndef RPN_HPP
 #define RPN_HPP
 
-class RPN {};
+#include <exception>
+#include <iostream>
+#include <stack>
+#include <string>
+
+class RPN {
+ public:
+  RPN();
+  ~RPN();
+  RPN(const RPN& copy);
+  RPN& operator=(const RPN& copy);
+  void calculate(std::string input);
+};
 
 #endif
