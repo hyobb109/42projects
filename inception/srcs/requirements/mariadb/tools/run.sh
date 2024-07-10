b9da31585b2320becb993d1f1078b748005b87a5
Hyobin Cho <hyobicho@student.42seoul.kr>
2024-07-10 23:10:26 +0900

    [inception] mariadb error

--- inception/srcs/requirements/mariadb/tools/run.sh
+++ inception/srcs/requirements/mariadb/tools/run.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 
-if [ ! -e "/run/mysqld/mysqld.sock" ]; then
-	mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db
-	mkdir -p /run/mysqld
-	mysqld &
+/usr/bin/mysqld --user=root --datadir=/var/lib/mysql &
 
-	while ! mysqladmin ping; do
-		sleep 1
-	done
+# mariaDB 서버 시작 전까지 대기
+while ! mysqladmin ping --silent; do
+	echo "Waiting for MariaDB to start..."
+	sleep 1
+done
 
-	mysql -uroot --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';"
-	mysql -uroot -p$MARIADB_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_ADMIN'@'%' IDENTIFIED BY '$WP_ADMIN_PASSWORD';"
-	mysql -uroot -p$MARIADB_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $WP_DB_NAME;"
-	mysql -uroot -p$MARIADB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-	mysqladmin -uroot -p$MARIADB_ROOT_PASSWORD shutdown
-fi
 
-exec mysqld
+mysql -uroot --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';"
+mysql -uroot -p$MARIADB_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $WP_DB_NAME;"
+mysql -uroot -p$MARIADB_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$WP_ADMIN'@'%' IDENTIFIED BY '$WP_ADMIN_PASSWORD';"
+mysql -uroot -p$MARIADB_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_ADMIN'@'%'"
+mysql -uroot -p$MARIADB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
